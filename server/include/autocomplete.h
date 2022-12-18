aa8d39e22fdf766904f3f902d7888f98d493777c
Daniel Smyth <daniel.smyth@tigerspider.com.au>
2022-12-19 01:51:24 +1100

    Update autocomplete.h

--- server/include/autocomplete.h
+++ server/include/autocomplete.h
@@ -6,9 +6,13 @@
 
 struct Node
 {
+    // The character stored in this node
     char character;
+    // Whether this character can be the end of a word
     bool isEnd;
+    // A counter indicating how many times a word is inserted
     int counter;
+    // dictionary of child nodes keys are characters, values are nodes
     std::map<const char, Node *> children;
 
     Node(char c);
@@ -16,12 +20,16 @@ struct Node
 
 class Trie
 {
+    // The trie has at least the root node. This has no character
     Node rootNode;
+    //  Array of matching words
     std::vector<std::pair<std::string, int>> output;
+
     void dfs(Node *node, std::string prefix);
 
 public:
     Trie();
+
     void insert(std::string word);
     std::vector<std::pair<std::string, int>> query(std::string query);
 };
